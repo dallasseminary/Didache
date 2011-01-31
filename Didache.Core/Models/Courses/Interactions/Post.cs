65e73d7ea28a177be9429fc978f5a171ca04a997
johndyer <johndyer@gmail.com>
2011-01-31 12:28:32 -0600

    - in intial interaction objects

---
+++ Didache.Core/Models/Courses/Interactions/Post.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Didache {
+
+
+	public class InteractionPost {
+		[Key]
+		public int PostID { get; set; }
+		public int ThreadID { get; set; }
+		public int TaskID { get; set; }
+		public bool IsApproved { get; set; }
+
+		public DateTime PostDate { get; set; }
+		public int UserID { get; set; }	
+		public int ReplyToPostID { get; set; }
+		public int TotalViews { get; set; }
+
+		public string Subject{ get; set; }
+		public string PostContent { get; set; }
+		public string PostContentFormatted { get; set; }
+
+		public virtual Thread Thread { get; set; }
+		public virtual User User { get; set; }
+	}
+
+
+
+}
