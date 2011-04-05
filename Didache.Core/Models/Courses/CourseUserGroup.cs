d342f39fe362652ece485dc6046fef6a8a4fda86
5iveblades <cjones@dts.edu>
2011-04-05 14:00:27 -0500

    - Updated models for CourseUserGroup and Courses to begin including Facilitators  - Changes to roster and interactions

--- Didache.Core/Models/Courses/CourseUserGroup.cs
+++ Didache.Core/Models/Courses/CourseUserGroup.cs
@@ -18,7 +18,7 @@ namespace Didache {
 		public string Name{ get; set; }
 
 		public virtual ICollection<CourseUser> Students { get; set;}
-		//public virtual Profile Facilitator;
+        public virtual User Facilitator { get; set; }
 
 	}
 }
