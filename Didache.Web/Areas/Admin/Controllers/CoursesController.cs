90666d4b5b7c0e76b30e1bdaf5afd9bd261d58cb
johndyer <johndyer@gmail.com>
2011-04-06 11:00:33 -0500

    - file system admin

--- Didache.Web/Areas/Admin/Controllers/CoursesController.cs
+++ Didache.Web/Areas/Admin/Controllers/CoursesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.IO;
 using Didache;
 
 namespace Didache.Web.Areas.Admin.Controllers
@@ -43,6 +44,168 @@ namespace Didache.Web.Areas.Admin.Controllers
 
 			return View(units);
 		}
+
+		public ActionResult Files(int id) {
+
+			List<CourseFileGroup> groups = CourseFiles.GetCourseFileGroups(id);
+			ViewBag.Course = Courses.GetCourse(id);
+
+			return View(groups);
+
+
+		}
+
+		[HttpPost]
+		public ActionResult AddFileGroup(int id) {
+
+			var didacheDb = new DidacheDb();
+
+			// get data
+			var reader = new JsonFx.Json.JsonReader();
+			dynamic json = reader.Read(HttpUtility.UrlDecode(Request.Form.ToString()));
+
+			CourseFileGroup group = new CourseFileGroup();
+			group.CourseID = id;
+			group.SortOrder = 9999;
+			group.Name = json.name;
+
+			didacheDb.CourseFileGroups.Add(group);
+			didacheDb.SaveChanges();
+
+
+			return Json(new {groupid = group.GroupID, name=json.name, courseid= id});
+		}
+
+
+		[HttpPost]
+		public ActionResult FileUpload(int id) {
+
+			// get groupID from form data JSON
+			int groupID = 0;			
+			string data = Request.Form["groupid"];
+			Int32.TryParse(data, out groupID);
+
+			// prep objects
+			var didacheDb = new DidacheDb();
+	
+			Guid uniqueID = Guid.NewGuid();
+			string originalFilename = "";
+			string originalExtension = "";
+			string title = "";
+			int fileID = 0;
+			HttpPostedFileBase file = null;
+
+			// save file
+			if (Request.Files.Count > 0 && Request.Files[0].ContentLength > 0) {
+				file = Request.Files[0];
+
+				originalFilename = Path.GetFileName(file.FileName);
+				originalExtension = Path.GetExtension(file.FileName);
+				title = originalFilename.Replace(originalExtension, ""); ;
+
+				string filePath = Path.Combine(HttpContext.Server.MapPath("~/uploads"), uniqueID.ToString() + originalExtension);
+				file.SaveAs(filePath);
+
+				CourseFile courseFile = new CourseFile();
+				courseFile.UserID = Users.GetLoggedInUser().UserID;
+				courseFile.UniqueID = uniqueID;
+				courseFile.ContentType = file.ContentType;
+				courseFile.Length = file.ContentLength;
+				courseFile.Filename = originalFilename;
+				courseFile.UploadedDate = DateTime.Now;
+				courseFile.Title = title;
+				courseFile.Description = "";
+
+				didacheDb.CourseFiles.Add(courseFile);
+				
+				didacheDb.SaveChanges();
+				fileID = courseFile.FileID;
+
+
+				// create association
+				CourseFileAssociation cfa = new CourseFileAssociation();
+				cfa.FileID = fileID;
+				cfa.GroupID = groupID;
+				cfa.SortOrder = 999;
+
+				didacheDb.CourseFileAssociations.Add(cfa);
+				didacheDb.SaveChanges();
+
+
+			}
+
+			// do processing
+			object returnObject = new {
+				success = (file != null),
+				fileid = fileID,
+				filelength = (file != null) ? file.ContentLength : 0,
+				filename = originalFilename,
+				title = title,
+				user = Users.GetLoggedInUser().FullName
+			};
+
+			return Json(returnObject);
+		}
+
+		[HttpPost]
+		public ActionResult UpdateFileSorting(int id) {
+
+			var didacheDb = new DidacheDb();
+
+			string data = HttpUtility.UrlDecode( Request.Form.ToString() );
+
+			//dynamic newValue = Newtonsoft.Json.JsonConvert.DeserializeObject<System.Dynamic.>(data);
+			var reader = new JsonFx.Json.JsonReader();
+			dynamic output = reader.Read(data);
+
+			foreach (var groupInfo in output) {
+
+				// get and update the group
+				CourseFileGroup fileGroup = didacheDb.CourseFileGroups.Find(groupInfo.groupid);
+				fileGroup.SortOrder = groupInfo.sortorder;
+				fileGroup.Name = groupInfo.name;
+				
+
+				// TEST 2: get all existing, update, remove missing
+
+
+				// get exsiting files
+				List<CourseFileAssociation> courseFiles = didacheDb.CourseFileAssociations.Where(cfa => cfa.GroupID == fileGroup.GroupID).ToList();
+				
+
+				foreach (var fileInfo in groupInfo.files) {
+					// find the file
+					CourseFileAssociation cfa = courseFiles.Find(c => c.FileID == fileInfo.fileid);
+					if (cfa != null) {
+						// update
+						cfa.SortOrder = fileInfo.sortorder;
+						// add to change list
+						//changedFiles.Add(cfa);
+						courseFiles.Remove(cfa);
+					} else {
+						cfa = new CourseFileAssociation();
+						cfa.GroupID = fileGroup.GroupID;
+						cfa.FileID = fileInfo.fileid;
+						cfa.SortOrder = fileInfo.sortorder;
+						didacheDb.CourseFileAssociations.Add(cfa);
+					}
+				}
+
+				// remove all remaining files
+				foreach (CourseFileAssociation notUpdated in courseFiles) {
+					didacheDb.CourseFileAssociations.Remove(notUpdated);
+				}
+
+			}
+
+			didacheDb.SaveChanges();
+
+
+			// need to deserialize this and update all the groups and files
+
+			return Json(new { success= true});
+		}
+
 
 		public ActionResult Unit(int? id, int? courseID) {
 			Unit unit = db.Units.SingleOrDefault(u => u.UnitID == id);
