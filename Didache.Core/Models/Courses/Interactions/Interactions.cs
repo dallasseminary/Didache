37cd98b3ea83766eff08145375996bac8e8b14ff
5iveblades <cjones@dts.edu>
2011-06-09 10:12:21 -0500

    - changes to Schedule view, Interactions  - limited displayed interactions to student's group

--- Didache.Core/Models/Courses/Interactions/Interactions.cs
+++ Didache.Core/Models/Courses/Interactions/Interactions.cs
@@ -7,12 +7,21 @@ namespace Didache {
 	public class Interactions {
 		
 		public static List<InteractionThread> GetInteractionThreads(int taskID) {
+            DidacheDb db = new DidacheDb();
+            User user = Users.GetLoggedInUser();
+            Task task = db.Tasks.Find(taskID);
+            Course course = Courses.GetCourse(task.CourseID);
+            CourseUser courseUser = course.CourseUsers.SingleOrDefault(u => u.UserID == user.UserID);
+            List<int> courseUserIDs = new List<int>();
+            foreach (CourseUser cu in course.CourseUsers) {
+                if (cu.GroupID == courseUser.GroupID) { courseUserIDs.Add(cu.UserID); }
+            }
 
-			List<InteractionThread> threads = new DidacheDb()
+			List<InteractionThread> threads = db
 				.InteractionThreads
 				.Include("Posts")
 				.Include("Posts.User")
-				.Where(t => t.TaskID == taskID)
+				.Where(t => t.TaskID == taskID && courseUserIDs.Contains(t.UserID))
 				.OrderByDescending(t => t.ThreadDate)
 				.ToList();
 
