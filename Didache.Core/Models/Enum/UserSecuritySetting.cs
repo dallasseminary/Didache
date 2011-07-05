8fd34ea88ace3080ac89360f63fe840ffb5300e9
johndyer <johndyer@gmail.com>
2011-07-05 15:40:58 -0500

    - security

---
+++ Didache.Core/Models/Enum/UserSecuritySetting.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Didache {
+	public enum UserSecuritySetting {
+		Private = 1,
+		Public = 2,
+		Friends = 3
+	}
+}
