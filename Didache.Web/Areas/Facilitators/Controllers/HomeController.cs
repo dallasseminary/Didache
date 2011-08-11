c099b1f2812b65dc8bc954e34195d1680bfda9fd
johndyer <johndyer@gmail.com>
2011-08-11 14:28:15 -0500

    - grading!

---
+++ Didache.Web/Areas/Facilitators/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Didache.Web.Areas.Facilitators.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Grading/Home/
+
+        public ActionResult Index2()
+        {
+			// get this users courses
+			List<Course> coursesAsFacilitator = Didache.Courses.GetUsersRunningCourses(CourseUserRole.Faciliator);
+			
+			// get all courses if administrator
+			List<Course> allCourses = User.IsInRole(UserRoles.Administrator) ?
+				Didache.Courses.GetCurrentlyRunningCourses() : coursesAsFacilitator;
+
+			ViewBag.CoursesAsFacilitator = coursesAsFacilitator;
+			ViewBag.Sessions = Sessions.GetSessions();
+
+			return View(allCourses);
+        }
+
+		public ActionResult Index(int? sessionID) {
+			
+			// all courses ever
+			List<Course> coursesAsFacilitator = null;
+			List<Course> sessionCourses = null;
+
+			if (sessionID.HasValue) {
+				// get all courses
+				coursesAsFacilitator = Didache.Courses.GetUsersCourses(CourseUserRole.Faciliator);
+
+
+				sessionCourses = (User.IsInRole(UserRoles.Administrator)) ?  
+							Didache.Courses.GetCoursesBySession(sessionID.Value) :
+							coursesAsFacilitator.Where(c => c.SessionID == sessionID.Value).ToList();
+			} else {
+
+				// just active courses 
+				coursesAsFacilitator = Didache.Courses.GetUsersRunningCourses(CourseUserRole.Faciliator);
+
+				sessionCourses = User.IsInRole(UserRoles.Administrator) ?
+							Didache.Courses.GetCurrentlyRunningCourses() : 
+							coursesAsFacilitator;
+
+			}
+
+													
+			ViewBag.Sessions = Sessions.GetSessions();
+			ViewBag.CoursesAsFacilitator = coursesAsFacilitator;
+
+			return View(sessionCourses);
+		}
+
+    }
+}
---
+++ Didache.Web/Areas/Facilitators/FacilitatorsAreaRegistration.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Mvc;
+
+namespace Didache.Web.Areas.Facilitators {
+	public class FacilitatorsAreaRegistration : AreaRegistration {
+		public override string AreaName {
+			get {
+				return "Facilitators";
+			}
+		}
+
+		public override void RegisterArea(AreaRegistrationContext context) {
+			
+			// grading home
+			context.MapRoute(
+				"Grading_default",
+				"grading/{sessionID}",
+				new { controller = "Home", action = "Index", sessionID = UrlParameter.Optional }
+			);
+
+			// task view
+			context.MapRoute(
+				"Grading_parts",
+				"grading/{slug}/{action}/{id}/{id2}",
+				new { controller = "Grading", action = "Index", id = UrlParameter.Optional, id2 = UrlParameter.Optional }
+			);
+
+
+		}
+	}
+}
